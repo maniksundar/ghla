acc409acfa47d85eca23900a663b8906a5d8f6c0
Sundar <sundara.ven@gmail.com>
2018-05-06 15:47:08 -0400

    Add dataModel for the Report, Metric and Question classes.

---
+++ app/src/main/java/Report.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+
+public class Report {
+    Metric metric;
+    Status status;
+    Report(Metric metric, Status status) {
+        this.metric = metric;
+        this.status = status;
+    }
+
+}
+
+class Metric{
+    MetricType type;
+    ArrayList<Question> questions;
+    enum MetricType{
+        Membership, ResourcesDaily, ResourcesMonthly, Extension, FinanceMonthly, FinanceWeekly, Monitoring, Meetings
+    }
+
+    Metric (MetricType type, ArrayList<Question> questions) {
+        this.type = type;
+        this.questions = questions;
+    }
+}
+
+class Status{
+    Value value;
+    enum Value{
+        NotStarted, Submitted, Approved
+    }
+}
+
+class Question {
+    String title;
+    String subTitle;
+    String questionText;
+
+    Question(String questionText) {
+        this.questionText = questionText;
+    }
+
+    Question(String title, String subTitle, String questionText) {
+        this.title = title;
+        this.subTitle = subTitle;
+        this.questionText = questionText;
+    }
+    Question(String title, String questionText) {
+        this.title = title;
+        this.questionText = questionText;
+    }
+
+}
+
+
