6bc518115b1c1045295de5e16f807e74fa4f7f8e
Sundar <sundara.ven@gmail.com>
2018-05-06 14:46:58 -0400

    Add bottom navigation activity - base activity and pair it with recycler view

--- app/src/main/java/com/ghla/library/authority/SignInActivity.java
+++ app/src/main/java/com/ghla/library/authority/SignInActivity.java
@@ -1,7 +1,9 @@
 package com.ghla.library.authority;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignInActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    public void loginClicked(View view) {
+        Intent intent = new Intent(this, BaseActivity.class);
+        startActivity(intent);
     }
 }
