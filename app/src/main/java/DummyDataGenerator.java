f646835bc51096837edcac5c701a562357573aa5
Sundar <sundara.ven@gmail.com>
2018-05-09 21:17:08 -0400

    Add methods for all types of reports

--- app/src/main/java/DummyDataGenerator.java
+++ app/src/main/java/DummyDataGenerator.java
@@ -1,19 +1,65 @@
 import java.util.ArrayList;
 
 public class DummyDataGenerator {
-    static int COUNT_REPORT = 10;
+    static int COUNT_REPORT = 1;
     static int COUNT_QUESTION = 10;
 
     ArrayList<Report> getReports(){
         ArrayList<Report> reports = new ArrayList<>();
         for (int i = 0; i < COUNT_REPORT; i++ ) {
             reports.add(generateMemberShipReport());
+            reports.add(generateMonitoringReport());
+            reports.add(generateFinanceMonthlyReport()); reports.add(generateFinanceWeeklyReport());
+            reports.add(generateResourcesDailyReport()); reports.add(generateResourcesMonthlyReport());
+            reports.add(generateExtensionReport());
         }
         return reports;
     }
 
     Report generateMemberShipReport(){
         Metric metric =  new Metric(Metric.MetricType.Membership, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateMonitoringReport(){
+        Metric metric =  new Metric(Metric.MetricType.Monitoring, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateExtensionReport(){
+        Metric metric =  new Metric(Metric.MetricType.Extension, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateResourcesMonthlyReport(){
+        Metric metric =  new Metric(Metric.MetricType.ResourcesMonthly, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateResourcesDailyReport(){
+        Metric metric =  new Metric(Metric.MetricType.ResourcesDaily, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateFinanceMonthlyReport(){
+        Metric metric =  new Metric(Metric.MetricType.FinanceMonthly, generateRandomQuestions());
+        Status status = new Status(Status.Value.NotStarted);
+        Report report = new Report(metric, status);
+        return report;
+    }
+
+    Report generateFinanceWeeklyReport(){
+        Metric metric =  new Metric(Metric.MetricType.FinanceWeekly, generateRandomQuestions());
         Status status = new Status(Status.Value.NotStarted);
         Report report = new Report(metric, status);
         return report;
