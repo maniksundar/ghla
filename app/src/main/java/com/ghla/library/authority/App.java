571bd56c9ebf1f26b715c231aa93eaac17cfb971
Sundar <sundara.ven@gmail.com>
2018-06-06 21:19:53 -0400

    add new app class that extends application

---
+++ app/src/main/java/com/ghla/library/authority/App.java
@@ -0,0 +1,19 @@
+package com.ghla.library.authority;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+
+    private static Context m_context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        m_context = getApplicationContext();
+    }
+
+    public static Context getContext() {
+        return m_context;
+    }
+}
