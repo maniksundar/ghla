a9bef919b37354a63964f1cd1c38a609ecbaf22d
Sundar <sundara.ven@gmail.com>
2018-06-12 00:45:16 -0400

    add view dynamically to reportContent

--- app/src/main/java/com/ghla/library/authority/MyReportContentRecyclerViewAdapter.java
+++ app/src/main/java/com/ghla/library/authority/MyReportContentRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ghla.library.authority.ReportContentFragment.OnListFragmentInteractionListener;
@@ -30,14 +31,32 @@ public class MyReportContentRecyclerViewAdapter extends RecyclerView.Adapter<MyR
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_reportcontent, parent, false);
+        LinearLayout linearLayout = (LinearLayout) view;
+        ViewGroup.LayoutParams lparams = new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TextView tv = new TextView(parent.getContext());
+        tv.setLayoutParams(lparams);
+        tv.setText("Subtitle");
+        linearLayout.addView(tv);
+
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mTitleView.setText(mTitles.get(position).text);
-        holder.mSubtitleView.setText(mTitles.get(position).text);
-        holder.mQuestionView.setText(mTitles.get(position).text);
+        holder.mSubtitleView.setText(mTitles.get(position).subtitles.get(0).text);
+        holder.mQuestionView.setText(mTitles.get(position).subtitles.get(0).questions.get(0).text);
+//        LayoutInflater vi = (LayoutInflater) App.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        View v = vi.inflate(R.layout.fragment_reportcontent, null);
+//        ViewGroup membership = v.findViewById(R.id.membership_parent);
+//        TextView textView = v.findViewById(R.id.membership_subtitle);
+//        textView.setText(mTitles.get(position).subtitles.get(1).text);
+//        membership.addView(textView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//
+//        textView = v.findViewById(R.id.membership_question);
+//        textView.setText(mTitles.get(position).subtitles.get(0).questions.get(1).text);
+//        membership.addView(textView, -1, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
     }
 
     @Override
