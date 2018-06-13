2d365154b230a38246a8d0b146f2ff37eaa48d28
Sundar <sundara.ven@gmail.com>
2018-06-12 23:56:59 -0400

    save the answers to the data model as they are entered in the membership report.

--- app/src/main/java/com/ghla/library/authority/MyReportContentRecyclerViewAdapter.java
+++ app/src/main/java/com/ghla/library/authority/MyReportContentRecyclerViewAdapter.java
@@ -2,16 +2,20 @@ package com.ghla.library.authority;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ghla.library.authority.ReportContentFragment.OnListFragmentInteractionListener;
 import com.ghla.library.authority.dummy.DummyContent.DummyItem;
 
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -45,7 +49,7 @@ public class MyReportContentRecyclerViewAdapter extends RecyclerView.Adapter<MyR
         for (Subtitle subtitle: title.subtitles){
             addSubtitleView(linearLayout, subtitle.text);
             for (Question question : subtitle.questions){
-                addQuestionView(linearLayout, question.text);
+                addQuestionView(linearLayout, question);
             }
         }
         if(position == mTitles.size()-1){
@@ -65,18 +69,42 @@ public class MyReportContentRecyclerViewAdapter extends RecyclerView.Adapter<MyR
         tv.setText(text);
         layout.addView(subtitleLayout);
     }
-    void addQuestionView (LinearLayout layout, String text) {
+    void addQuestionView (LinearLayout layout, Question question) {
         LayoutInflater vi = (LayoutInflater) App.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View questionLayout = vi.inflate(R.layout.report_content_question, null);
         TextView tv = questionLayout.findViewById(R.id.text);
-        tv.setText(text);
+        tv.setText(question.text);
+        EditText answerEdit = questionLayout.findViewById(R.id.answerEdit);
+        answerEdit.addTextChangedListener(new AnswerWatcher(question));
         layout.addView(questionLayout);
     }
 
     void addSubmitButtonToLastTitle(LinearLayout layout) {
         LayoutInflater vi = (LayoutInflater) App.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         View buttonLayout = vi.inflate(R.layout.report_content_submit, null);
+        Button button = buttonLayout.findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ready to submit the form
+                // Send the Membership report to the Network layer
+                System.out.println(mTitles);
+            }
+        });
         layout.addView(buttonLayout);
+    }
+
+    void setAnswer(Object object, Integer answer){
+        if (object.getClass() == Question.class){
+            Question obj = (Question) object;
+            obj.answer = answer;
+        } else if (object.getClass() == Question.class){
+            Subtitle obj = (Subtitle) object;
+            obj.answer = answer;
+        } else if (object.getClass() == Question.class){
+            Title obj = (Title) object;
+            obj.answer = answer;
+        }
     }
 
     @Override
@@ -91,6 +119,30 @@ public class MyReportContentRecyclerViewAdapter extends RecyclerView.Adapter<MyR
         public ViewHolder(View view) {
             super(view);
             mView = view;
+        }
+    }
+
+    class AnswerWatcher implements TextWatcher {
+
+        Object dataModel;
+
+        AnswerWatcher(Object dataModel){
+            this.dataModel = dataModel;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            setAnswer(dataModel, Integer.parseInt(s.toString()));
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
         }
     }
 }
